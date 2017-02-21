b3367841933ea6e99dab32427b11080bd48c26e5
Matt Spurlin <mspurlin@ics-llc.net>
2017-02-20 21:45:45 -0600

    Remove duplication for starts.

--- app/docker/start.sh
+++ app/docker/start.sh
@@ -2,6 +2,5 @@ sleep 30
 echo "  database: $DB_NAME" >> /usr/share/nginx/app/config/database.yml
 echo "  username: $DB_USER" >> /usr/share/nginx/app/config/database.yml
 echo "  password: $DB_PASS" >> /usr/share/nginx/app/config/database.yml
-cd /usr/share/nginx/app && bash -l -c "bundle exec rake db:migrate RAILS_ENV=production"
 cd /usr/share/nginx/app && bash -l -c "bundle exec rake db:seed RAILS_ENV=production"
 /usr/sbin/nginx -g 'daemon off;'
